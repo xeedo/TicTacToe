2f7ed6f95c445713eb873f171d430a74f86959ea
Przemysław Grądzik <pggradzik@gmail.com>
2017-09-20 22:29:52 +0200

    Added win condition. First playable version.

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,105 @@
-function toggle(event) {
-  // document.getElementById("demo").innerHTML = cell.innerHTML;
+// Setup global variables
 
+var playerTurn = "X";
+var currentState = {
+  "one": "",
+  "two": "",
+  "three": "",
+  "four": "",
+  "five": "",
+  "six": "",
+  "seven": "",
+  "eight": "",
+  "nine": ""
+};
+
+// ------
+// Functions
+
+function toggle(event) {
   var cell = event.target;
 
+  // Make sure that trigers only on cell
+  if (cell.tagName == "TH") {
+    makeMove(cell);
+  }
+
+  // Check if game is over
+  if (isGameOver()) {
+    document.getElementById("demo").innerHTML = playerTurn + " has won!";
+    resetGame(); 
+  }
+
+  changePlayer();
+}
+
+function makeMove(cell) {
+  // make move only on empty cell
   if (cell.innerHTML == "") {
-    cell.innerHTML = "X";
+    cell.innerHTML = playerTurn;
+    currentState[cell.id] = playerTurn;
   }
-  else if (cell.innerHTML == "X") {
-    cell.innerHTML = "O";
+}
+
+function changePlayer() {
+  if (playerTurn == "X") {
+    playerTurn = "O";
   }
-  else if (cell.innerHTML = "O") {
-    cell.innerHTML = ""
+  else {
+    playerTurn = "X";
+  }
+}
+
+function isGameOver() {
+  var aCells = document.getElementsByTagName("TH");
+  var isOver = true;
+
+  if (isWin()) {
+    return isOver;
+  }
+  else {
+    for (cell of aCells) {
+      if (cell.innerHTML == "") {
+        isOver = false;
+      }
+    }
+    return isOver;
+  }
+}
+
+function isWin() {
+  var winStates = {
+    1: ["one", "two", "three"],
+    2: ["four", "five", "six"],
+    3: ["seven", "eight", "nine"],
+    4: ["one", "four", "seven"],
+    5: ["two", "five", "eight"],
+    6: ["three", "six", "nine"],
+    7: ["one", "five", "nine"],
+    8: ["three", "five", "seven"]
+  };
+
+  var states = "";
+  var win = false;
+
+  for (var state in winStates) {
+    states = "";
+
+    for (var i = 0; i < winStates[state].length; i++) {
+      states += currentState[winStates[state][i]];
+    }
+
+    if (states == "XXX" || states == "OOO") {
+      win = true;
+      break;
+    }
+  }
+
+  return win;
+}
+
+function resetGame() {
+  for (cell of document.getElementsByTagName("TH")) {
+    cell.innerHTML = "";
   }
 }
